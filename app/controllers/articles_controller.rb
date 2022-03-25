13247190d2e0fd75fbff9116f9fad20880629833
Arek <Akaczkowski97@gmail.com>
2022-03-25 09:49:58 -0400

    Article save with private method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,25 @@
 class ArticlesController < ApplicationController
   
   def show
-    byebug
     @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save!
+  end
+
+  private 
+
+    def article_params 
+      params.require(:article).permit(:title, :description)
+    end
 end
