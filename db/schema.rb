9702499c4d86b2cb077598de2b4425e6c9399f5e
Arek <Akaczkowski97@gmail.com>
2022-04-15 17:08:18 -0400

    Add user validations

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_01_212603) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
