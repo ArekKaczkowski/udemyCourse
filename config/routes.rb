fef2722ea3ab33bcbac81f203293e98b8d4e712e
Arek <Akaczkowski97@gmail.com>
2022-05-04 18:11:38 -0400

    Add ability to edit users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
    root "pages#home"
    get 'about', to: "pages#about"
-
    resources :articles
+   get 'signup', to: 'users#new'
+   resources :users, except: [:new]
 end
